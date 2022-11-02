7c05934ee0e115b02f26329c8343fc1772bf5528
malrang <ruehd7924@gmail.com>
2022-11-02 15:28:56 +0900

    ➕[add]: Json singletone 파일추가및 정의

---
+++ Covid inoculation center/Covid inoculation center/Sources/Utils/Singletone/Json.swift
@@ -0,0 +1,15 @@
+//
+//  Json.swift
+//  Covid inoculation center
+//
+//  Created by 김동욱 on 2022/11/02.
+//
+
+import Foundation
+
+struct Json {
+    static let encoder = JSONEncoder()
+    static let decoder = JSONDecoder()
+
+    private init() {}
+}
