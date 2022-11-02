4fbfc1ff99b5fd6a29d33129037d1083669fcab6
malrang <ruehd7924@gmail.com>
2022-11-02 15:15:42 +0900

    ➕[add]: 앱 라이브러리 추가및 스토리보드 제거, 파일트리 변경

---
+++ Covid inoculation center/Covid inoculation centerUITests/Covid_inoculation_centerUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Covid_inoculation_centerUITestsLaunchTests.swift
+//  Covid inoculation centerUITests
+//
+//  Created by 김동욱 on 2022/11/02.
+//
+
+import XCTest
+
+final class Covid_inoculation_centerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
